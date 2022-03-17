d1aab1e5326ba3cde409652a20d692f72b1ee31b
Kim Tae Hyun <97386129+tjdrkr2580@users.noreply.github.com>
2022-03-17 20:10:37 +0900

    clock timer C

---
+++ js/app.js
@@ -0,0 +1,74 @@
+const clock = document.querySelector(".momentum_layout .clock");
+const todoinput = document.querySelector(".todo_input");
+const form = document.querySelector("form");
+const timeText = document.querySelector(".time_text");
+
+
+setInterval(function(){
+    let timer = new Date();
+    let h = String(timer.getHours()).padStart(2,"0");
+    switch (h) {
+        case "6":
+            timeText.innerText = "Good morning , user";
+            break
+        case "7":
+            timeText.innerText = "Good morning , user";
+            break;
+        case "8":
+            timeText.innerText = "Good morning , user";
+            break;
+        case "9":
+            timeText.innerText = "Good morning , user";
+            break;
+        case "10":
+            timeText.innerText = "Good morning , user";
+            break
+        case "11":
+            timeText.innerText = "Good morning , user";
+            breakm
+        case "12":
+            timeText.innerText = "Good morning , user";
+            break;
+        case "13":
+            timeText.innerText = "Good afternoon , user";
+            break;
+        case "14":
+            timeText.innerText = "Good afternoon , user";
+            break;
+        case "15":
+            timeText.innerText = "Good afternoon , user";
+            break;
+        case "16":
+            timeText.innerText = "Good afternoon , user";
+            break;
+        case "17":
+            timeText.innerText = "Good evening , user";
+            break;
+        case "18":
+            timeText.innerText = "Good evening , user";
+            break;
+        case "19":
+            timeText.innerText = "Good evening , user";
+            break;
+        case "20":
+            timeText.innerText = "Good evening , user";
+            break;
+        case "21":
+            timeText.innerText = "Good evening , user";
+            break;
+
+        default:
+            timeText.innerText = "Good night , user";
+            break;
+ }
+    let m = String(timer.getMinutes()).padStart(2,"0");
+    let s = String(timer.getSeconds()).padStart(2,"0");
+    clock.innerText = `${h}:${m}:${s}`;
+},1000);
+
+function submitTodo(e){
+   e.preventDefault(); 
+}
+
+
+form.addEventListener("submit",submitTodo);
